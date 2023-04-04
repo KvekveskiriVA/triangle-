e4acf015698c1a94309b35a5bb27ab20ed607bb6
Vladislav <nerokvent7@gmail.com>
2023-04-04 12:20:26 +0300

    456

--- triangle/Program.cs
+++ triangle/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (1 < 2) 
+{
+    Console.WriteLine("Введите размер треугольника");
+    var storona = Convert.ToInt32(Console.ReadLine());
+    var simbol = Console.ReadLine();
+    var simbolFillNo = Console.ReadLine();
+    //Int32.Parse(Console.ReadLine());
+    var simbolCount = 0;
+    var simbolCountMin= 0;
+    var simbolCountMax= 0;
+
+    var counter = 0;
+    var storonaStop = 0; 
+    for (var j = 1; j < storona; j++)
+    {
+        simbolCount = (storona / 2) - j + 1;
+        simbolCountMin = (storona / 2) - j + 1;
+        simbolCountMax = simbolCount + j;
+
+        for (; counter < simbolCountMin; counter++)
+        {
+            Console.Write(simbolFillNo);
+        }
+
+        var gran = simbolCountMax + (simbolCountMax - counter - 1);
+        for (; counter < gran; counter++)
+        {
+            Console.Write(simbol);
+            storonaStop++;
+            if (storona == storonaStop)
+            {
+                j = storona;
+            }
+        }
+
+        for (; counter < storona + 1; counter++)
+        {
+            Console.Write(simbolFillNo);
+        }
+        storonaStop = 0;
+        counter = 0;
+        Console.WriteLine();
+
+    }
+}
